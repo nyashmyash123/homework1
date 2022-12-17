21e67351afc968872c12ae672d507c311b2e8099
Nikita <makhonin_n_s@mail.ru>
2022-12-17 19:20:19 +0400

    дз2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        var boxerWeight = 78.2;
+        var boxer2Weight = 82.7;
+        var boxersWeight = boxerWeight + boxer2Weight;
+        System.out.println("Общий вес боксеров" + boxersWeight);
+        var boxersWeight2 =  boxerWeight - boxer2Weight;
+        System.out.println("разница в весе боксеров" + boxersWeight2);
+        var boxersWeight3 = boxer2Weight - boxerWeight;
+        System.out.println("разница в весе боксеров" + boxersWeight3);
+
+
+
+
+
+
     }
 }
